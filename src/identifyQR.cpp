caf7815b6ab201d1fc745f42f31313a83cdbf033
Witnessthis <emil.j.granberg@gmail.com>
2016-06-15 14:14:57 +0200

    added image and fixed drawing of the QR codes

--- src/identifyQR.cpp
+++ src/identifyQR.cpp
@@ -27,47 +27,44 @@
    // wrap image data  
    Image image(width, height, "Y800", raw, width * height);  
    // scan the image for barcodes  
-   int n = scanner.scan(image);  
+   scanner.scan(image);
    // extract results  
    for(Image::SymbolIterator symbol = image.symbol_begin();  
      symbol != image.symbol_end();  
      ++symbol) {  
-                vector<Point> vp;  
+                vector<Point> vp;
      // do something useful with results  
      cout << "decoded " << symbol->get_type_name()  
         << " symbol \"" << symbol->get_data() << '"' <<" "<< endl;  
            int n = symbol->get_location_size();  
            for(int i=0;i<n;i++){  
                 vp.push_back(Point(symbol->get_location_x(i),symbol->get_location_y(i))); 
-           }  
-           RotatedRect r = minAreaRect(vp);  
-           Point2f pts[4];  
-           r.points(pts);
-           for(int i=0;i<4;i++){
+           }
 
-               if(i == 0){
-                    botLength = sqrt(pow(pts[i].x-pts[(i+1)%4].x, 2) + pow(pts[i].y-pts[(i+1)%4].y, 2));
-                    cout << "botLength: " << botLength << endl;
-               }
-               else if (i == 1){
-                    leftLength = sqrt(pow(pts[i].x-pts[(i+1)%4].x, 2) + pow(pts[i].y-pts[(i+1)%4].y, 2));
-                    cout << "leftLength: " << leftLength << endl;
-               }
-               else if (i == 2){
-                    topLength = sqrt(pow(pts[i].x-pts[(i+1)%4].x, 2) + pow(pts[i].y-pts[(i+1)%4].y, 2));
-                    cout << "topLength: " << topLength << endl;
-               }
-               else if (i == 3){
-                    rightLength = sqrt(pow(pts[i].x-pts[(i+1)%4].x, 2) + pow(pts[i].y-pts[(i+1)%4].y, 2));
-                    cout << "rightLength: " << rightLength << endl;
-               }
+       for(int i=0;i<vp.size();i++){
 
-//               cout << "pts[i]: " << pts[i] << endl;
-//               cout << "pts[(i+1)%4]: " << pts[(i+1)%4] << endl;
+           if(i == 0){
+               botLength = sqrt(pow(vp[i].x-vp[(i+1)%4].x, 2) + pow(vp[i].y-vp[(i+1)%4].y, 2));
+               cout << "botLength: " << botLength << endl;
+           }
+           else if (i == 1){
+               leftLength = sqrt(pow(vp[i].x-vp[(i+1)%4].x, 2) + pow(vp[i].y-vp[(i+1)%4].y, 2));
+               cout << "leftLength: " << leftLength << endl;
+           }
+           else if (i == 2){
+               topLength = sqrt(pow(vp[i].x-vp[(i+1)%4].x, 2) + pow(vp[i].y-vp[(i+1)%4].y, 2));
+               cout << "topLength: " << topLength << endl;
+           }
+           else if (i == 3){
+               rightLength = sqrt(pow(vp[i].x-vp[(i+1)%4].x, 2) + pow(vp[i].y-vp[(i+1)%4].y, 2));
+               cout << "rightLength: " << rightLength << endl;
+           }
 
-               line(imgout,pts[i],pts[(i+1)%4],Scalar(255,0,0),3);
-           }  
-           cout<<"Angle: "<<r.angle<<endl;  
+           cout << "pts[i].x: " << vp[i].x << endl;
+           cout << "pts[i].y: " << vp[i].y << endl;
+
+           line(imgout,vp[i],vp[(i+1)%4],Scalar(255,0,0),3);
+       }
    }  
       imshow("imgout.jpg",imgout);  
    // clean up  
